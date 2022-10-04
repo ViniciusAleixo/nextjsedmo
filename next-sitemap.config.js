cfbd7db4d2a3208fea913622f70156dfd12f548c
Administrator <vinicius_aleixo@yahoo.com.br>
2022-10-04 12:38:56 +1030

    add all files 021

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,7 @@ const siteUrl = "https://products.edmoap.com.au";
 module.exports = {
     siteUrl,
     generateRobotsTxt: true,
+    sitemapSize: 1000,
     exclude: ['/server-sitemap.xml'],
     robotsTxtOptions: {
         additionalSitemaps: [
